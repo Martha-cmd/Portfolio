9cf8d85a7a708f952c6cbce1f3528180b00e4835
Martha-cmd <agumartha053@gmail.com>
2024-06-14 19:53:09 +0100

    responsiveness-ii

--- src/pages/Home/Copyright.jsx
+++ src/pages/Home/Copyright.jsx
@@ -4,7 +4,7 @@ import { LuCopyright } from "react-icons/lu";
 const Copyright = () => {
   return (
     <>
-         <main className='w-full flex justify-between lightgrey mt-8 px-5 text-[13px] font-light'>
+         <main className='w-full flex justify-between lightgrey mt-8 text-[13px] font-light lg:px-[180px] md:px-[50px] px-[20px]'>
 
             <div className='flex items-center gap-1'>
                 <p><LuCopyright size={10}/></p>
